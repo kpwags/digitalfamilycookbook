f8e35f13c61c1f33a241e4604347d832e1eeea65
Keith Wagner <keith@kpwags.com>
2019-03-10 17:16:35 -0400

    Initial setup complete(ish)

---
+++ frontend/lib/withData.js
@@ -0,0 +1,26 @@
+import withApollo from 'next-with-apollo';
+import ApolloClient from 'apollo-boost';
+import { endpoint } from '../config';
+
+function createClient({ headers }) {
+    return new ApolloClient({
+        uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+        request: operation => {
+            operation.setContext({
+                fetchOptions: {
+                    credentials: 'include'
+                },
+                headers
+            });
+        },
+        // local data
+        clientState: {
+            resolvers: {
+                Mutation: {}
+            },
+            defaults: {}
+        }
+    });
+}
+
+export default withApollo(createClient);
