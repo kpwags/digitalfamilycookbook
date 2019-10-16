61f7655644fb6bc7b4cf78f49f2196da560ca5c9
Keith Wagner <keith@kpwags.com>
2019-10-15 20:15:11 -0400

    Corrected Document
    Corrected the code for _document.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,14 +3,10 @@ import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
     static getInitialProps({ renderPage }) {
-        try {
-            const sheet = new ServerStyleSheet();
-            const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
-            const styleTags = sheet.getStyleElement();
-            return { ...page, styleTags };
-        } catch (e) {
-            console.log({ e });
-        }
+        const sheet = new ServerStyleSheet();
+        const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+        const styleTags = sheet.getStyleElement();
+        return { ...page, styleTags };
     }
 
     render() {
