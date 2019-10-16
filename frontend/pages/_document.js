8fa4e66952c4bf558e6463ffc1b68c99d59ded88
Keith Wagner <keith@kpwags.com>
2019-10-15 20:13:41 -0400

    Updated Packages
    Updated packages to support Next 9.1

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,10 +3,14 @@ import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
     static getInitialProps({ renderPage }) {
-        const sheet = new ServerStyleSheet();
-        const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
-        const styleTags = sheet.getStyleElement();
-        return { ...page, styleTags };
+        try {
+            const sheet = new ServerStyleSheet();
+            const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+            const styleTags = sheet.getStyleElement();
+            return { ...page, styleTags };
+        } catch (e) {
+            console.log({ e });
+        }
     }
 
     render() {
