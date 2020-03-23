7e2648e3979e566d595938bc4f5d5cf8c34fef3c
Keith Wagner <keith@kpwags.com>
2020-03-23 19:22:26 -0400

    Converted Meats Mobile Nav to React Hooks
    Converted Meats mobile nav over to hooks

--- frontend/components/MeatsMobileNav/MeatsMobileNav.js
+++ frontend/components/MeatsMobileNav/MeatsMobileNav.js
@@ -1,85 +1,70 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
+import { useState } from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import Link from 'next/link';
 import { ALL_MEATS_QUERY } from '../../queries/Meat';
 import { Utilities } from '../../lib/Utilities';
 
-class MeatsMobileNav extends Component {
-    toggleMenu = () => {
-        if (document.getElementById('meats').style.display !== 'none') {
-            document.getElementById('meats').style.display = 'none';
-            document.getElementById('meats-arrow').className = 'arrow right';
-        } else {
-            document.getElementById('meats').style.display = 'block';
-            document.getElementById('meats-arrow').className = 'arrow down';
-        }
-    };
+const MeatsMobileNav = () => {
+    const [expanded, setExpanded] = useState(false);
 
-    render() {
-        return (
-            <>
-                <li className="title bordered">
-                    <a
-                        role="button"
-                        tabIndex="0"
-                        onClick={e => {
-                            e.preventDefault();
-                            this.toggleMenu();
-                        }}
-                        onKeyDown={e => {
-                            e.preventDefault();
-                            if (e.keyCode === 13 || e.keyCode === 32) {
-                                this.toggleMenu();
-                            }
-                        }}
-                    >
-                        <i id="meats-arrow" className="arrow right" />
-                        Meats
-                    </a>
-                </li>
-                <li>
-                    <ul id="meats" style={{ display: 'none' }}>
-                        <Query query={ALL_MEATS_QUERY}>
-                            {({ data, error, loading }) => {
-                                if (loading) return <li />;
-                                if (error) return <></>;
+    const { data, loading, error } = useQuery(ALL_MEATS_QUERY);
 
-                                return data.meats.length > 0 ? (
-                                    data.meats.map(meat => (
-                                        <li key={meat.id}>
-                                            <Link href={`/meat?id=${meat.id}`}>
-                                                <a
-                                                    role="button"
-                                                    tabIndex="0"
-                                                    onClick={() => {
-                                                        Utilities.toggleMobileMenu();
-                                                    }}
-                                                    onKeyDown={e => {
-                                                        if (
-                                                            e.keyCode === 13 ||
-                                                            e.keyCode === 32
-                                                        ) {
-                                                            Utilities.toggleMobileMenu();
-                                                        }
-                                                    }}
-                                                >
-                                                    {meat.name}
-                                                </a>
-                                            </Link>
-                                        </li>
-                                    ))
-                                ) : (
-                                    <li>
-                                        <em>No Meats Defined</em>
-                                    </li>
-                                );
-                            }}
-                        </Query>
-                    </ul>
-                </li>
-            </>
-        );
-    }
-}
+    if (loading || error) return <li />;
+
+    return (
+        <>
+            <li className="title bordered">
+                <a
+                    role="button"
+                    tabIndex="0"
+                    onClick={e => {
+                        e.preventDefault();
+                        setExpanded(!expanded);
+                    }}
+                    onKeyDown={e => {
+                        e.preventDefault();
+                        if (e.keyCode === 13 || e.keyCode === 32) {
+                            setExpanded(!expanded);
+                        }
+                    }}
+                >
+                    <i id="meats-arrow" className={expanded ? 'arrow down' : 'arrow right'} />
+                    Meats
+                </a>
+            </li>
+            <li>
+                <ul id="meats" style={expanded ? { display: 'block' } : { display: 'none' }}>
+                    {data.meats.length > 0 &&
+                        data.meats.map(meat => (
+                            <li key={meat.id}>
+                                <Link href={`/meat?id=${meat.id}`}>
+                                    <a
+                                        role="button"
+                                        tabIndex="0"
+                                        onClick={() => {
+                                            Utilities.toggleMobileMenu();
+                                        }}
+                                        onKeyDown={e => {
+                                            if (e.keyCode === 13 || e.keyCode === 32) {
+                                                Utilities.toggleMobileMenu();
+                                            }
+                                        }}
+                                    >
+                                        {meat.name}
+                                    </a>
+                                </Link>
+                            </li>
+                        ))}{' '}
+                    : (
+                    {data.meats.length === 0 && (
+                        <li>
+                            <em>No Meats Defined</em>
+                        </li>
+                    )}
+                </ul>
+            </li>
+        </>
+    );
+};
 
 export { MeatsMobileNav };
